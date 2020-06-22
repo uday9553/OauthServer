1611003a1bd2062cc26b3b98918914d7f5b666c0
uday <uday9553@outlook.com>
2020-06-22 11:12:54 +0530

    initial commit

---
+++ src/main/java/com/uday/configuration/UserConfiguration.java
@@ -0,0 +1,20 @@
+package com.uday.configuration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    PasswordEncoder passwordEncoder=PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication().withUser("uday").password(passwordEncoder.encode("uday123")).roles("USER","ADMIN","MANAGER").authorities("CAN_READ","CAN_WRITE","CAN_DELETE").and()
+                .withUser("sagar").password(passwordEncoder.encode("sagar123")).roles("USER").authorities("CAN_READ","CAN_WRITE");
+    }
+}
